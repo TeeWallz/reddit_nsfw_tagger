15fc5c99619ddf4a4b8bf4d673027d9860483fcd
Tomek Waller <tom.waller@versent.com.au>
2022-01-01 22:02:51 +1100

    pages test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/reddit_nsfw_tagger/' : ''
+  assetPrefix: isProd ? '/reddit_nsfw_tagger/' : '',
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
 }
